154cfb1f1d2289ea08cb9dee15c0667c60aaf8dc
kmarks2013 <kmarks2013@gmail.com>
2020-05-10 14:27:00 -0400

    fixes import statements in app.js

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
-import { createAppContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
 import HomeScreen from './src/screens/HomeScreen'
 
 const navigator = createStackNavigator({
