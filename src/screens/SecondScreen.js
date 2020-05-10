65c257c8ea1f5c08fd49fb87c9ab8a064e29b707
kmarks2013 <kmarks2013@gmail.com>
2020-05-10 15:47:16 -0400

    adds second screen.js

---
+++ src/screens/SecondScreen.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+
+export default class FirstScreen extends Component {
+    render() {
+        return (
+            <View>
+                <Text> Navigating like a pro! </Text>
+            </View>
+        )
+    }
+}
