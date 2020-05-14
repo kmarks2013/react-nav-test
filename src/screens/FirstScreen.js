dfaad3e0ea0b1f548a48a8ba5e9415bdebe60295
kmarks2013 <kmarks2013@gmail.com>
2020-05-14 12:28:23 -0400

    adds first screen

---
+++ src/screens/FirstScreen.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+
+export default class FirstScreen extends Component {
+    render() {
+        return (
+            <View>
+                <Text> You made it to the first screen! </Text>
+            </View>
+        )
+    }
+}
