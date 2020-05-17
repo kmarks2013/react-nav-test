fdcf1db055d23fba79ec519919bd8fac742dc052
kmarks2013 <kmarks2013@gmail.com>
2020-05-17 09:29:45 -0400

    adds form screen

---
+++ src/screens/FormExample.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const FormExample = () => {
+    return (
+        <View>
+            <Text>Test form</Text>
+        </View>
+    )
+}
+
+export default FormExample
